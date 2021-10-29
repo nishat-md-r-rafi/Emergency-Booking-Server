feba0f4099f3f0eed4568c8852ea64140d644eb3
unknown <r.rafi1615005@gmail.com>
2021-10-29 11:08:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const cors = require("cors");
+require("dotenv").config();
+
+// create app and port
+const app = express();
+const port = 5000;
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+
+//connect to DB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mqo07.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// create client
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("emergencyBooking");
+    const serviceCollection = database.collection("services");
+  } finally {
+    // await client.close()
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running Emergency Server Very Cool");
+});
+
+app.listen(port, () => {
+  console.log("listening to port", port);
+});
